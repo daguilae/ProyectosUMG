37d5523b65049c6cb86de9d46ded60a5c6f10192
zuzu-ai <sucelyalvarez@hotmail.com>
2020-05-12 16:31:48 -0600

    Login

--- P3GA1/Prototipo_2P-master/Prototipo2P/src/prototipo2p/Asignacion_cursos_alumno.java
+++ P3GA1/Prototipo_2P-master/Prototipo2P/src/prototipo2p/Asignacion_cursos_alumno.java
@@ -19,7 +19,7 @@ public class Asignacion_cursos_alumno extends javax.swing.JInternalFrame {
      */
     String BD = "jdbc:mysql://localhost/siu";
     String Usuario = "root";
-    String Clave = "Cagada1234";
+    String Clave = "6182";
     
     
     public Asignacion_cursos_alumno() {
