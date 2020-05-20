8211fa0dbf327ef813e5710846bac4eeae8e9dd2
Ashly2015 <ranbr200015@gmail.com>
2020-05-20 09:56:56 -0600

    Merge

--- P3GA3/Parcial2/src/Principal.java
+++ P3GA3/Parcial2/src/Principal.java
@@ -31,7 +31,7 @@ public class Principal extends javax.swing.JFrame {
 
     public static String BD = "jdbc:mysql://localhost/siu";
     public static String Usuario = "root";
-    public static String Contraseña = "Langas798";
+    public static String Contraseña = "Polo.2015";
 
     /**
      * Creates new form Principal
