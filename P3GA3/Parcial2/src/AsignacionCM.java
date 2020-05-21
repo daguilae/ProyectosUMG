c111be4c5799c401cdaaaff2ab82a9dca46a195e
Ashly2015 <ranbr200015@gmail.com>
2020-05-20 19:59:46 -0600

    Validacion de cursos con carreras

--- P3GA3/Parcial2/src/AsignacionCM.java
+++ P3GA3/Parcial2/src/AsignacionCM.java
@@ -128,9 +128,7 @@ public class AsignacionCM extends javax.swing.JInternalFrame {
             PreparedStatement pst5 = cn.prepareStatement("select nombre_aula from aulas");
             ResultSet rs5 = pst5.executeQuery();
 
-            PreparedStatement pst6 = cn.prepareStatement("select nombre_curso from cursos");
-            ResultSet rs6 = pst6.executeQuery();
-
+            
             PreparedStatement pst7 = cn.prepareStatement("select nombre_maestro from maestros");
             ResultSet rs7 = pst7.executeQuery();
 
@@ -160,9 +158,7 @@ public class AsignacionCM extends javax.swing.JInternalFrame {
             }
 
             cbox_curso.addItem("Seleccione una opción");
-            while (rs6.next()) {
-                cbox_curso.addItem(rs6.getString("nombre_curso"));
-            }
+            
 
             cbox_alum.addItem("Seleccione una opción");
             while (rs7.next()) {
@@ -528,6 +524,16 @@ public class AsignacionCM extends javax.swing.JInternalFrame {
 
             } else {
 
+            }
+            cbox_curso.removeAllItems();
+            cbox_curso.addItem("Seleccione una opción");
+            PreparedStatement pst6 = cn.prepareStatement("select nombre_curso from cursos where codigo_carrera=?");
+            pst6.setString(1, lb1.getText());
+            ResultSet rs6 = pst6.executeQuery();
+            
+           
+            while (rs6.next()) {
+                cbox_curso.addItem(rs6.getString("nombre_curso"));
             }
 
         } catch (Exception e) {
